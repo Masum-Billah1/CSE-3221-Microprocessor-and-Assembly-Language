b6f607b62944eeb8d9b8ae57bb3d78b6fb4c5eab
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2023-04-04 00:41:45 +0600

    Reverse_String_with_Stack

--- String/1b_reversestack.asm
+++ String/1b_reversestack.asm
@@ -1,34 +1,55 @@
 .model small
-.stack 100h
+.stack 100h 
+.data 
+    str db 100 ?
+    msg1 db "Enter the string:$"
+    msg2 db "Reversed string:$"
 .code
-main proc
+main proc  
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9 
+    lea dx,msg1 
+    int 21h  
+    
     xor cx,cx
-    mov ah,1
-    int 21h
+    input:
+        mov ah,1
+        int 21h
+        cmp al,13 
+        je end_input
+        push ax
+        inc cx
+    jmp input 
     
-    while:
-    cmp al,0dh
-    je end_while
-    push ax
-    inc cx
-    int 21h
-    jmp while
-    end_while:
+    end_input:
     
+    call new 
+    
+    mov ah,9
+    lea dx,msg2
+    int 21h 
+   
+    jcxz exit;first enter key handle
     mov ah,2
-    mov dl,0dh
-    int 21h
-    mov dl, 0ah
-    int 21h
-    jcxz exit
-    
-    top:
-    pop dx
-    int 21h
-    loop top
+    output: 
+        pop dx 
+        int 21h
+    loop output
     
     exit:
-    mov ah,4ch
-    int 21h
-    main endp
+        mov ah,4ch
+        int 21h   
+    main endp 
+
+    new proc 
+        mov ah,2
+        mov dl,10
+        int 21h
+        mov ah,2
+        mov dl,13
+        int 21h
+        ret
+    new endp
 end main
