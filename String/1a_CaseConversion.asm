c26e57e3c3e4874eee233cb8e9ad3dc96ada22b1
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2023-07-10 18:49:14 +0600

    Update 1a_CaseConversion.asm

--- String/1a_CaseConversion.asm
+++ String/1a_CaseConversion.asm
@@ -1,72 +1,68 @@
-.model small
-.stack 100h 
+ .model small
+.stack 100h
 .data 
-    str db 100 ?
-    msg1 db "Enter the string:$"
-    msg2 db "Output:$"
+    msg1 db 'Enter the string:$'
+    msg2 db 'Case Conversion:$'
+    str db 100 dup ('$')  
+   
 .code
-main proc  
+main proc 
     mov ax,@data
-    mov ds,ax
+    mov ds,AX
     
-    mov ah,9 
-    lea dx,msg1 
-    int 21h  
-    
+    mov ah,9
+    lea dx,msg1
+    int 21h 
     mov si,0
     
-    input:
-        mov ah,1
-        int 21h
-        cmp al,13
-        je end_input
-        
-        cmp al,'A'
-        jl next
-        
-        cmp al,'z'
-        jg next
-        
-        cmp al,'Z'
-        jg check 
-        
-        jmp end_check
-        
-        check:
-            cmp al,'a'
-            jl next
-        end_check: 
-            
-        xor al,32
-        
-        next:
-           mov str[si],al 
-           inc si;
-       
-    jmp input 
-    
-    end_input:
-    
-    mov ah,2
-    mov dl,10
+    input:    
+    mov ah,1
     int 21h
-    mov ah,2
-    mov dl,13
-    int 21h 
+    cmp al,13
+    je next 
+    cmp al,'A' 
+    jl same
+    cmp al,'z'
+    jg same
     
+    cmp al,'Z'
+    jg check
+    jmp end_check
+    check:
+    cmp al,'a'
+    jl same
+    
+    end_check:
+    xor ax,32    
+    same:
+    mov str[si],al 
+    inc si
+    jmp input
+        
+    
+    next:
+    call new
     mov ah,9
     lea dx,msg2
     int 21h
-    
-    output: 
-    cmp di,si
-    je end 
-    mov ah,2 
-    mov dl,str[di]
+    mov ah,9
+    lea dx,str
     int 21h
-    inc di
-    jmp output
-    
-    end:   
-    main endp
+   
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp   
+
+new proc 
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov ah,2 
+    mov dl,13
+    int 21h
+    ret
+    new endp
+
+        
 end main
